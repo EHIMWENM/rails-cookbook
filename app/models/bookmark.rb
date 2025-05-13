1d41d8f5d181ff95c7f0d367d4d144513e66df04
Ehimwenma Odiase <odiwem@gmail.com>
2025-05-13 12:58:35 +0100

    validate

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
-  validates [:recipe, :category] uniqueness: true
-  validates {:comment "cannot be shorter than 6 characters"}
+  validates :recipe_id, uniqueness: { scope: :category_id }
+  validates :comment, length: { minimum: 6, too_short: "must have at least 6 characters" }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true, uniqueness: true
-  validates :rating(6.9), presence: true
+  validates :description, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
